90bb3940e667d0e1f08ebf74214c4ccb4b9dc007
bilalYousuf-petcircle <bilal.yousuf@petcircle.com.au>
2025-07-23 14:18:09 +0500

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <AutomateProcess />
       <Services />
       <Waves />
-      <OurTeam />
+      {/* <OurTeam /> */}
       <Pricing />
       <Cta />
       <About />
